69f5caa8031e1c7d7268ec8ac82eef817f9bd427
gahee <sarahan774@gmail.com>
2020-07-15 16:39:27 +0900

    Add publish activity UI

--- app/src/main/java/com/forif/honsullife/ui/LoginActivity.java
+++ app/src/main/java/com/forif/honsullife/ui/LoginActivity.java
@@ -2,7 +2,10 @@ package com.forif.honsullife.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.forif.honsullife.R;
 
@@ -10,10 +13,26 @@ public class LoginActivity extends AppCompatActivity {
 
     /* 구글 로그인 구현하기 */
 
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        btnLogin = findViewById(R.id.btn_google_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO 1. load spinner 시작하기
+                //2. 인증정보 저장하기 (?)
+
+                //3. 로그인 완료되면 화면 넘어가기
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
 }
--- app/src/main/java/com/forif/honsullife/ui/MainActivity.java
+++ app/src/main/java/com/forif/honsullife/ui/MainActivity.java
@@ -1,31 +1,13 @@
 package com.forif.honsullife.ui;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ContentResolver;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.provider.Settings;
 import android.view.View;
-import android.webkit.MimeTypeMap;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.forif.honsullife.R;
-import com.google.android.gms.tasks.Continuation;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,9 +16,22 @@ public class MainActivity extends AppCompatActivity {
     * 3. 다른 팀 포스트 작성 시간 순으로 세로로 띄우기
     * 4. AppBar 에다가 로그아웃, 랭킹보기 메뉴 넣기
     * */
+
+    private FloatingActionButton fabNavigateToPublish;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fabNavigateToPublish = findViewById(R.id.fab_navigate_to_publish_activity);
+        fabNavigateToPublish.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PublishPostActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
