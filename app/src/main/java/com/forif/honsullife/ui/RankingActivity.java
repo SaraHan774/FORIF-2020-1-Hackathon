fa78c4b1bdc3c2e4fac0e6bf4b48acd4f4aedd0f
gahee <sarahan774@gmail.com>
2020-07-18 14:19:37 +0900

    Make Rank UI

--- app/src/main/java/com/forif/honsullife/ui/RankingActivity.java
+++ app/src/main/java/com/forif/honsullife/ui/RankingActivity.java
@@ -10,9 +10,13 @@ public class RankingActivity extends AppCompatActivity {
 
     /*Firebase 에서 랭킹 정보 가져와서 랭킹 띄워주기.
     * 1, 2, 3등은 ViewPager 로 상단에, 나머지 팀은 RecyclerView 혹은 ListView 로 하단에 띄우기 */
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
+
+
     }
 }
