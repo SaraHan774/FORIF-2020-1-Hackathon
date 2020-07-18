c0b357a6699e677dd4ca8c6b64874a8fe063fb7e
iraira7777@naver.com <iraira7777@naver.com>
2020-07-18 18:10:23 +0900

    Editing View holder

--- app/src/main/java/com/forif/honsullife/model/OtherRvAdapter.java
+++ app/src/main/java/com/forif/honsullife/model/OtherRvAdapter.java
@@ -16,13 +16,13 @@ import com.google.android.material.card.MaterialCardView;
 
 import java.util.List;
 
-public class RvAdapter extends RecyclerView.Adapter<RvAdapter.ContentsViewHolder> {
+public class OtherRvAdapter extends RecyclerView.Adapter<OtherRvAdapter.ContentsViewHolder> {
 
     private Context mContext;
     private List<Post> mPosts;
     private OnItemClickListener mListener;
 
-    public RvAdapter(Context context, List<Post> mPosts) {
+    public OtherRvAdapter(Context context, List<Post> mPosts) {
         this.mContext = context;
         this.mPosts = mPosts;
     }
@@ -31,12 +31,14 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.ContentsViewHolder
 
         private ImageView mThumbnail;
         private TextView mTitle;
+        private TextView mPreview;
         private MaterialCardView mCard;
 
         public ContentsViewHolder(@NonNull View itemView) {
             super(itemView);
             mThumbnail = itemView.findViewById(R.id.other_team_image_view);
             mTitle = itemView.findViewById(R.id.tv_other_team_post_title);
+            mPreview = itemView.findViewById(R.id.tv_other_team_post_preview);
             mCard = itemView.findViewById(R.id.other_team_post_card);
             mCard.setOnClickListener(this);
         }
@@ -56,14 +58,23 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.ContentsViewHolder
     @Override
     public ContentsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(mContext);
-        View v =inflater.inflate(R.layout.others_post_view_holder,parent,false);
+        View v =inflater.inflate(R.layout.post_other_team_view_holder_v2,parent,false);
         return new ContentsViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ContentsViewHolder holder, int position) {
         Post postCur = mPosts.get(position);
+
+//        if(postCur.getPostContent().length() > 20){
+//            preview = postCur.getPostContent().substring(0,19) + "...";
+//        } else {
+//            preview = postCur.getPostContent();
+//        }
+
         holder.mTitle.setText(postCur.getPostName() + "\n"+postCur.getCreatedAt());
+        holder.mPreview.setText(postCur.getPostContent());
+
         Glide.with(mContext)
                 .load(postCur.getPhotoUrl())
                 .centerCrop()
